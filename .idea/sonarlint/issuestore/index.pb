762cdb1ac1332c605724a1d3d044d54e3bd60547
Brad (김용건) <yonggeon.kim@gmail.com>
2024-01-08 23:50:36 +0900

    [] feat: chater 2 feat: [] chater 2 - finish chapter 2

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -20,3 +20,15 @@ Q
 -.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
 S
 #.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+G
+src/main/kotlin/Todo.kt,e/1/e1696398ce4e0885a7b2bde1df86659b3619b9ff
+K
+src/main/kotlin/TodoList.kt,c/1/c16857e6b7b9dead76f81b8cf2bb0fd0af5c655f
+N
+src/main/kotlin/domain/User.kt,8/f/8f1b7e0e4a3169832aed6897d9d58eddb842aee8
+R
+"src/main/kotlin/domain/TodoList.kt,f/a/fa28933c6598a301a3f09d22f6a0fee881ae3817
+O
+src/main/kotlin/service/Todo.kt,8/8/88026f614d7ea35f06ce522042997e5b60a0d16e
+O
+src/main/kotlin/web/HtmlPage.kt,c/6/c6bce4d8bde75d577a22082d009fab2d45c4cd69
--- src/test/kotlin/SeeATodoListAT.kt
+++ src/test/kotlin/SeeATodoListAT.kt
@@ -1,7 +1,62 @@
-//import org.junit.jupiter.api.Test
-//
-//class SeeATodoListAT {
-//
-//    @Test
-//    fun `List owner
-//}
+import org.example.domain.ListName
+import org.example.domain.TodoItem
+import org.example.service.Todo
+import org.example.domain.TodoList
+import org.example.domain.User
+import org.http4k.client.JettyClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Status
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class SeeATodoListAT {
+
+    @Test
+    fun `List owners can see their lists`() {
+        val user="brad"
+        val listName="shopping"
+        val foodToBuy=listOf("carrots","apples","milk")
+        startApplication(user,listName,foodToBuy)
+        val list=getTodoList(user,listName)
+        expectThat(list.listName.name).isEqualTo(listName)
+        expectThat(list.items.map(TodoItem::description)).isEqualTo(foodToBuy)
+    }
+}
+
+fun startApplication(
+    user:String, listName:String, items:List<String>
+) {
+    val todoList = TodoList(ListName(listName), items.map(::TodoItem))
+    val lists = mapOf(User(user) to listOf(todoList))
+    val server = Todo(lists).asServer(Jetty(9091)).start()
+}
+
+fun getTodoList(user:String,listName: String): TodoList {
+    val client=JettyClient()
+    val request=Request(Method.GET, "http://localhost:9091/todo/$user/$listName")
+    val response = client(request)
+    return if (response.status== Status.OK) parseResponse(response.bodyString())
+    else fail(response.toMessage())
+}
+
+// API 응답인 HTML을 TodoList로 되돌린다.
+private fun parseResponse(html: String): TodoList {
+    val nameRegex = "<h2>.*<".toRegex()
+    val listName = ListName(extractListName(nameRegex, html))
+    val itemsRegex = "<td>.*?<".toRegex()
+    val items = itemsRegex.findAll(html)
+        .map { TodoItem(extractItemDesc(it)) }.toList()
+    return TodoList(listName,items)
+}
+private fun extractListName(nameRegex: Regex, html: String): String =
+    nameRegex.find(html)?.value
+        ?.substringAfter("<h2>")
+        ?.dropLast(1)
+        .orEmpty()
+private fun extractItemDesc(matchResult: MatchResult): String =
+    matchResult.value.substringAfter("<td>").dropLast(1)
