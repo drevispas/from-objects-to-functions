dba22c659522a824d50d552f4abf877be4afc863
Brad (김용건) <yonggeon.kim@gmail.com>
2024-01-11 22:14:23 +0900

    Correct incorrect answer

--- src/main/kotlin/domain/FunStack.kt
+++ src/main/kotlin/domain/FunStack.kt
@@ -1,19 +1,17 @@
 package org.example.domain
 
-data class FunStack<T>(val stack: List<T> = mutableListOf<T>()) {
+data class FunStack<T>(val stack: List<T> = emptyList()) {
 
     fun size() = stack.size
 
     fun push(a: T): FunStack<T> {
-        val newStack = FunStack<T>(stack.toMutableList())
-        newStack.stack.addFirst(a)
+        val newStack = FunStack(listOf(a) + stack)
         return newStack
     }
 
     fun pop(): Pair<T,FunStack<T>> {
-        val newStack = FunStack<T>(stack.toMutableList())
-        val b = newStack.stack[0]
-        newStack.stack.removeFirst()
+        val b = stack.first()
+        val newStack = FunStack(stack.drop(1))
         return b to newStack
     }
 }
