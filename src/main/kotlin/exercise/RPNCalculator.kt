ea777899ae556fc574d8f45c57859744e62be8aa
Brad (김용건) <yonggeon.kim@gmail.com>
2024-01-18 17:36:37 +0900

    Correct the answer

--- src/main/kotlin/exercise/RPNCalculator.kt
+++ src/main/kotlin/exercise/RPNCalculator.kt
@@ -2,11 +2,27 @@ package org.example.exercise
 
 class RPNCalculator {
 
-    val operationMap: Map<String, Pair<Double,Double>> = TODO()
+    val operationMap: Map<String, (Double, Double) -> Double> = mapOf(
+        "+" to Double::plus,
+        "-" to Double::minus,
+        "*" to Double::times,
+        "/" to Double::div
+    )
 
-    val funStack: FunStack<Double> = TODO()
+    val funStack: FunStack<Double> = FunStack()
 
-    fun calc(expr: String): Double = TODO()
+    // "5 6 2 1 + / * "
+    fun calc(expr: String): Double = expr.split(" ").fold(funStack, ::reduce).pop().first
 
-    private fun reduce(stack: FunStack<Double>, token: String): FunStack<Double> = TODO()
+    private fun reduce(stack: FunStack<Double>, token: String): FunStack<Double> =
+        if (operationMap.containsKey(token)) {
+            val (opr1, tempStack) = stack.pop()
+            val (opr2, newStack) = tempStack.pop()
+            newStack.push(operate(token, opr2, opr1))
+        } else {
+            stack.push(token.toDouble())
+        }
+
+    private fun operate(token: String, a: Double, b: Double): Double =
+        operationMap[token]?.invoke(a, b) ?: error("Invalid operator `$token`!")
 }
