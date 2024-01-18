35b8a1058c175dd462c3ef9a876a54da2b39a86a
Brad (김용건) <yonggeon.kim@gmail.com>
2024-01-18 16:50:07 +0900

    Skeleton

--- src/main/kotlin/exercise/RPNCalculator.kt
+++ src/main/kotlin/exercise/RPNCalculator.kt
@@ -2,28 +2,11 @@ package org.example.exercise
 
 class RPNCalculator {
 
+    val operationMap: Map<String, Pair<Double,Double>> = TODO()
 
-    fun calc(e: String): Double {
-        var funStack = FunStack<Double>()
-        val ops = e.split(" ")
-        ops.forEach {
-            val number = it.toDoubleOrNull()
-            println("n = $number")
-            funStack = if (number != null) {
-                funStack.push(number)
-            } else {
-                val (operand1, newFunStack1) = funStack.pop()
-                val (operand2, newFunStack2) = newFunStack1.pop()
-                val temp = when(it) {
-                    "+" -> operand2 + operand1
-                    "-" -> operand2 - operand1
-                    "*" -> operand2 * operand1
-                    "/" -> operand2 / operand1
-                    else -> error("Unsupported operator `it`!")
-                }
-                newFunStack2.push(temp)
-            }
-        }
-        return funStack.pop().first
-    }
+    val funStack: FunStack<Double> = TODO()
+
+    fun calc(expr: String): Double = TODO()
+
+    private fun reduce(stack: FunStack<Double>, token: String): FunStack<Double> = TODO()
 }
