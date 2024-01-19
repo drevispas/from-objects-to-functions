80897ee256520a8678b13f54c63382f14a75b156
Brad (김용건) <yonggeon.kim@gmail.com>
2024-01-19 12:22:51 +0900

    Test with scenario actor interface (#9)

--- src/test/kotlin/SeeATodoListAT.kt
+++ src/test/kotlin/SeeATodoListAT.kt
@@ -1,8 +1,8 @@
 import org.example.domain.ListName
 import org.example.domain.TodoItem
-import org.example.service.Todo
 import org.example.domain.TodoList
 import org.example.domain.User
+import org.example.service.Todo
 import org.http4k.client.JettyClient
 import org.http4k.core.Method
 import org.http4k.core.Request
@@ -14,33 +14,36 @@ import org.junit.jupiter.api.fail
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
+/**
+ * Way of chapter 2
+ */
 class SeeATodoListAT {
 
     @Test
     fun `List owners can see their lists`() {
-        val user="brad"
-        val listName="shopping"
-        val foodToBuy=listOf("carrots","apples","milk")
-        startApplication(user,listName,foodToBuy)
-        val list=getTodoList(user,listName)
+        val userName = "brad"
+        val listName = "shopping"
+        val foodToBuy = listOf("carrots", "apples", "milk")
+        startApplication(userName, listName, foodToBuy)
+        val list = getTodoList(userName, listName)
         expectThat(list.listName.name).isEqualTo(listName)
         expectThat(list.items.map(TodoItem::description)).isEqualTo(foodToBuy)
     }
 }
 
 fun startApplication(
-    user:String, listName:String, items:List<String>
+    userName: String, listName: String, itemNames: List<String>
 ) {
-    val todoList = TodoList(ListName(listName), items.map(::TodoItem))
-    val lists = mapOf(User(user) to listOf(todoList))
-    val server = Todo(lists).asServer(Jetty(9091)).start()
+    val todoList = TodoList(ListName(listName), itemNames.map(::TodoItem))
+    val userMap = mapOf(User(userName) to listOf(todoList))
+    val server = Todo(userMap).asServer(Jetty(9091)).start()
 }
 
-fun getTodoList(user:String,listName: String): TodoList {
-    val client=JettyClient()
-    val request=Request(Method.GET, "http://localhost:9091/todo/$user/$listName")
+fun getTodoList(user: String, listName: String): TodoList {
+    val client = JettyClient()
+    val request = Request(Method.GET, "http://localhost:9091/todo/$user/$listName")
     val response = client(request)
-    return if (response.status== Status.OK) parseResponse(response.bodyString())
+    return if (response.status == Status.OK) parseResponse(response.bodyString())
     else fail(response.toMessage())
 }
 
@@ -51,12 +54,46 @@ private fun parseResponse(html: String): TodoList {
     val itemsRegex = "<td>.*?<".toRegex()
     val items = itemsRegex.findAll(html)
         .map { TodoItem(extractItemDesc(it)) }.toList()
-    return TodoList(listName,items)
+    return TodoList(listName, items)
 }
+
 private fun extractListName(nameRegex: Regex, html: String): String =
     nameRegex.find(html)?.value
         ?.substringAfter("<h2>")
         ?.dropLast(1)
         .orEmpty()
+
 private fun extractItemDesc(matchResult: MatchResult): String =
     matchResult.value.substringAfter("<td>").dropLast(1)
+
+/**
+ * Way of chapter 3.1
+ */
+
+private fun createTodo(userName: String, listName: String, itemNames: List<String>) =
+    Todo(mapOf(User(userName) to listOf(TodoList(ListName(listName), itemNames.map(::TodoItem)))))
+
+fun startApplication(todo: Todo) = todo.asServer(Jetty(9091)).start()
+interface ScenarioActor {
+    val name: String
+}
+
+class UserTodoListItems(override val name: String) : ScenarioActor {
+
+    fun canGetItemsOfATodoList(userName: String, listName:String, itemNames: List<String>) {
+        val expected = TodoList(ListName(listName), itemNames.map(::TodoItem))
+        val actual = getTodoList(userName, listName)
+        expectThat(actual).isEqualTo(expected)
+    }
+}
+
+class TodoItemsTest {
+
+    @Test
+    fun `User gets the items of a todo list`() {
+        val todo = createTodo("brad", "shopping", listOf("carrot","milk"))
+        startApplication(todo)
+        val brad = UserTodoListItems("brad")
+        brad.canGetItemsOfATodoList("brad", "shopping", listOf("carrot","milk"))
+    }
+}
